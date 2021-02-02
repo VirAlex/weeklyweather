b45a5d2f425eb17abeb9498b2216ff2f223914bf
Alexandre <alexou73@gmail.com>
2021-02-02 08:37:06 +0100

    add env file and structure component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <weather-card/>
   </div>
 </template>
-
+    
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherCard from './components/WeatherCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherCard
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Weather Card</h1>
+</template>
+
+<script>
+export default {
+  name: "WeatherCard",
+  mounted() {
+    console.log('je sui monted')
+    this.test();
+  },
+  methods: {
+    test() {
+      console.log('ENV',process.env.VUE_APP_API_KEY);
+    },
+  },
+};
+</script>
+
+<style></style>
