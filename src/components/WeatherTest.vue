ae88feb0ecd6453bccc5453bc14d85a2b1f9660f
Alexandre <alexou73@gmail.com>
2021-02-03 10:37:29 +0100

    end image integration general

--- src/components/WeatherTest.vue
+++ src/components/WeatherTest.vue
@@ -4,18 +4,21 @@
     :style="'background-image:url(' + backAfternon + ')'"
   >
     <div class="container-weather">
-      <div class="city">PARIS</div>
+      <div class="city">{{ city }}</div>
+      <button @click="getMyInfo()">MA METEO</button>
       <div class="prevision">
         <div class="text">
           It's
           <br />
-          raining
+          {{ weather }}
           <br />
           now
         </div>
-        <div class="img"><img :src="suny" alt="" /></div>
+        <div class="img">
+          <img :src="iconWeather" alt="">
+        </div>
       </div>
-      <div class="temp">10°</div>
+      <div class="temp">{{ temperature }}°</div>
     </div>
   </div>
 </template>
@@ -24,6 +27,7 @@
 import backAfternon from "@/assets/after_noon.png";
 import backNight from "@/assets/night.jpg";
 import suny from "@/assets/icons/suny.png";
+import cloud from "@/assets/icons/cloud_force.png";
 import axios from "axios";
 
 export default {
@@ -32,9 +36,37 @@ export default {
       backAfternon,
       backNight,
       suny,
-      temp: '',
-
+      cloud,
+      temp: "",
+      location: null,
+      gettingLocation: false,
+      errorStr: null,
+      city: "",
+      temperature: "",
+      weather: "",
+      iconWeather: "",
     };
+  },
+  created() {
+    //do we support geolocation
+    if (!("geolocation" in navigator)) {
+      this.errorStr = "Geolocation is not available.";
+      return;
+    }
+
+    this.gettingLocation = true;
+    // get position
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.gettingLocation = false;
+        this.location = pos;
+        console.log("LOCATION", pos);
+      },
+      (err) => {
+        this.gettingLocation = false;
+        this.errorStr = err.message;
+      }
+    );
   },
   mounted() {
     this.getInfo();
@@ -45,7 +77,54 @@ export default {
         .get(
           `http://api.openweathermap.org/data/2.5/weather?q=Paris&&units=metric&appid=${process.env.VUE_APP_API_KEY}`
         )
-        .then((response) => console.log(response.data));
+        .then((response) => {
+          console.log("NO COORD", response.data);
+          this.city = response.data.name;
+          this.temperature = Math.round(response.data.main.temp);
+          this.weather = response.data.weather[0].main;
+          console.log("WEATHER", response.data.weather[0].icon);
+          switch (response.data.weather[0].icon) {
+            case "01d":
+              this.iconWeather = this.suny;
+              break;
+            case "02d":
+              this.iconWeather = '<i class="fas fa-cloud-sun"></i>';
+              break;
+            case "03d":
+              this.iconWeather = this.suny;
+              break;
+            case "04d":
+               this.iconWeather = this.cloud;
+              break;
+          }
+        });
+    },
+    getMyInfo() {
+      if (this.location) {
+        console.log("IF", this.location);
+        axios
+          .get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${this.location.coords.latitude}&lon=${this.location.coords.longitude}&units=metric&appid=${process.env.VUE_APP_API_KEY}`
+          )
+          .then((response) => {
+            console.log("COORD", response.data);
+            this.city = response.data.name;
+            this.temperature = Math.round(response.data.main.temp);
+            this.weather = response.data.weather[0].main;
+          });
+      } else {
+        console.log("ELSE", this.location);
+        axios
+          .get(
+            `http://api.openweathermap.org/data/2.5/weather?q=Paris&&units=metric&appid=${process.env.VUE_APP_API_KEY}`
+          )
+          .then((response) => {
+            console.log("NO COORD", response.data);
+            this.city = response.data.name;
+            this.temperature = Math.round(response.data.main.temp);
+            this.weather = response.data.weather[0].main;
+          });
+      }
     },
   },
 };
@@ -59,7 +138,7 @@ export default {
 }
 .main-container {
   width: 350px;
-  height: 650px;
+  height: 610px;
   margin: 0 auto;
   background-position: center;
   background: no-repeat;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faSearch, faBars, faStickyNote, faCalendar, faCog, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faSearch, faBars, faStickyNote, faCalendar, faCog, faPlusSquare, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret, faSearch, faBars, faStickyNote, faCalendar, faCog, faPlusSquare)
+library.add(faUserSecret, faSearch, faBars, faStickyNote, faCalendar, faCog, faPlusSquare, faSun)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
